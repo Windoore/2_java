50a1cd6171aebd42dc9eef4152966fc308faa7dd
Windoore <fadeev_dima2001@mail.ru>
2020-10-06 20:03:17 +0300

    me
    creat files

--- src/Java_2.java
+++ src/Java_2.java
@@ -1,5 +1,27 @@
 public class Java_2 {
     public static void main(String[] args) {
-        
+        int[][] array = new int[6][7];
+        for (int i = 0; i <6; i++) {
+            for (int j = 0; j <7; j++) {
+                array[i][j] = (int)(Math.random()*10);
+
+            }
+        }
+        for (int i = array.length-1; i>=0; i--) {
+            int max=0;
+            for (int j = 0; j <7; j++) {
+
+                if (array[i][j] > max){
+                   max=array[i][j];
+                }
+            }
+            array[i][0]=max;
+        }
+        for (int i = 0; i <6; i++) {
+            for (int j = 0; j <7; j++) {
+                System.out.printf(" "+"%d",array[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
