137a55c1e4d35ea51cbe7cbff6a7ce8470cfa62e
Windoore <fadeev_dima2001@mail.ru>
2020-10-07 15:13:51 +0300

    me

--- src/Java_2.java
+++ src/Java_2.java
@@ -1,27 +1,37 @@
 public class Java_2 {
     public static void main(String[] args) {
         int[][] array = new int[6][7];
+      full(array);
+      print(array);
+
+
+    }
+    public static int full (int[][] x){
         for (int i = 0; i <6; i++) {
             for (int j = 0; j <7; j++) {
-                array[i][j] = (int)(Math.random()*10);
+                x[i][j] = (int)(Math.random()*10);
 
             }
         }
-        for (int i = array.length-1; i>=0; i--) {
-            int max=0;
+        int max=0;
+        for (int i = x.length-1; i>=0; i--) {
             for (int j = 0; j <7; j++) {
 
-                if (array[i][j] > max){
-                   max=array[i][j];
+                if (x[i][j] > max){
+                    max=x[i][j];
                 }
             }
-            array[i][0]=max;
+            x[i][0]=max;
         }
+        return max;
+    }
+    public static void print (int[][] x){
         for (int i = 0; i <6; i++) {
             for (int j = 0; j <7; j++) {
-                System.out.printf(" "+"%d",array[i][j]);
+                System.out.printf(" "+"%d",x[i][j]);
             }
             System.out.println();
         }
     }
+
 }
